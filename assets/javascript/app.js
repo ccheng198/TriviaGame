98a9cd071c4946d487f69e8d42cb8ddc496505b2
ccheng198 <ccheng198@gmail.com>
2017-10-20 21:35:23 -0500

    eh

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// when the "done" button is hit, stop the timer and go to finish screen
+
+//setTimeout for timer on main page
+// 60000 ms
+// 5 multiple questions with 4 options per question
+//put the function with correct/incorrect/unanswered inside of setTimeout function
+// put the function with all the radio buttons withing a function of the start button
+
+// var countdown = 
+// var correct =
+// var incorrect =
+// var unanswered =
+
+// $("#timeRemaining").html("Time Remaining" + countdown)
